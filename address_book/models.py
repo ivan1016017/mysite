61316ef387660f13e2cf6ccced99bdb1a1f70a29
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-24 17:32:29 -0600

    new comment added in models.py

--- address_book/models.py
+++ address_book/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# this is a new comment
 class Address(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
